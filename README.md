c65baac15ebf868b5f0355dd0816d21c3ae115bf
AhmedMohamed797 <elpopaboesmail@example.com>
2026-08-18 00:41:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## E-Commerce FreshCart
+
+# Packages
+
+[1] Tailwind ✅
+[2] React Router ✅
+[3] Axios ✅
+[4] Font Source ✅
+[5] Font Awesome ✅
+[6] Formik ✅
+[7] Yup ✅
---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import "@fontsource/poppins";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas, far, fab);
+
+import { createBrowserRouter, Navigate } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Layout from "./components/Layout/Layout";
+import Brands from "./pages/Brands/Brands";
+import Cart from "./pages/Cart/Cart";
+import Categories from "./pages/Categories/Categories";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/NotFound/NotFound";
+import Orders from "./pages/Orders/Orders";
+import Signup from "./pages/Signup/Signup";
+import Wishlist from "./pages/Wishlist/Wishlist";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to={"/home"} />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "brands",
+          element: <Brands />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+        {
+          path: "categories",
+          element: <Categories />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "signup",
+          element: <Signup />,
+        },
+        {
+          path: "orders",
+          element: <Orders />,
+        },
+        {
+          path: "wishlist",
+          element: <Wishlist />,
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
