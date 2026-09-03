077f9debf3cbae474e1648bcf3edb6726fcabd1d
AhmedMohamed797 <elpopaboesmail@example.com>
2026-09-03 18:28:48 +0300

    Handle Scroll restoration

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration } from "react-router";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
@@ -8,6 +8,7 @@ export default function Layout() {
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </>
   );
 }
